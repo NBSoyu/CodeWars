339fb5db6b0689ebd6206f8f5f2555522d6eaae2
Soyu <43044531+NBSoyu@users.noreply.github.com>
2022-02-26 04:09:53 +0300

    1st
    -

---
+++ patch.js
@@ -0,0 +1,41 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// function makeNegative(a) {
+// if(a>0){
+//   return -a;
+// }
+//   else if(num<0){
+//     return a;
+//   }
+//   else
+//     return 0;
+//   }
+
+
+
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// function XO(str) {
+//     let x = 0, 
+//         o = 0;
+  
+//     for (let i = 0; i < str.length; i++) { 
+//         if (str[i].toLowerCase() === "x") {
+//             x++;
+//         } else if (str[i].toLowerCase() === "o") {
+//             o++;
+//         }
+//     }
+//     return x === o;
+// }
+
+
+
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// const reverseSeq = n => {
+// let arr = [];
+//   for (let i=n; i>0; i--) {
+//     arr.push(i);
+//     } return arr;
+// };
